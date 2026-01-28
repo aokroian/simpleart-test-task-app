10673213aebdd5ac84fcc59e831907926ea483b3
aokroian <49920714+aokroian@users.noreply.github.com>
2026-01-28 21:04:23 +0300

    Update CarouselAutoSwiper.cs
    improved auto swiper so that it will not interrupt player input

--- Assets/__Game/Scripts/UI/Utils/CarouselAutoSwiper.cs
+++ Assets/__Game/Scripts/UI/Utils/CarouselAutoSwiper.cs
@@ -1,7 +1,9 @@
 using System;
 using DanielLochner.Assets.SimpleScrollSnap;
 using R3;
+using R3.Triggers;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace UI.Utils
 {
@@ -10,11 +12,39 @@ namespace UI.Utils
         [SerializeField] private float intervalSeconds = 5f;
         [SerializeField] private SimpleScrollSnap simpleScrollSnap;
 
+        private readonly ReactiveProperty<bool> _isMoving = new();
+        private IDisposable _swipeSub;
+
         private void Start()
         {
-            Observable.Interval(TimeSpan.FromSeconds(intervalSeconds))
-                .Subscribe(_ => simpleScrollSnap.GoToNextPanel())
+            _isMoving.DistinctUntilChanged()
+                .Subscribe(isMoving =>
+                {
+                    _swipeSub?.Dispose();
+                    if (!isMoving)
+                        ScheduleAutoSwipe();
+                })
                 .AddTo(this);
+            simpleScrollSnap.GetComponent<ScrollRect>()
+                .OnBeginDragAsObservable()
+                .Subscribe(_ => { _isMoving.Value = true; });
+            simpleScrollSnap.GetComponent<ScrollRect>()
+                .OnEndDragAsObservable()
+                .Subscribe(_ => { _isMoving.Value = false; });
+        }
+
+        private void ScheduleAutoSwipe()
+        {
+            _swipeSub = Observable.Timer(TimeSpan.FromSeconds(intervalSeconds))
+                .Subscribe(_ =>
+                {
+                    if (_isMoving.Value)
+                    {
+                        return;
+                    }
+                    simpleScrollSnap.GoToNextPanel();
+                    ScheduleAutoSwipe();
+                });
         }
     }
 }
